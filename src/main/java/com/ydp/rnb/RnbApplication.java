f230654336016693f1a72da8f1247f5d34d27bb7
Leonardo Pilastri <leonardo.pilastri@sonarsource.com>
2023-03-07 10:45:58 +0100

    fisrt

---
+++ src/main/java/com/ydp/rnb/RnbApplication.java
@@ -0,0 +1,13 @@
+package com.ydp.rnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RnbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ydp/rnb/RnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydp.rnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
